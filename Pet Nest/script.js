94619316cdaaea61bd4af1f59820e1c4d43992f8
Aayush Nishad <146199188+AayushNishad@users.noreply.github.com>
2023-10-19 20:41:29 +0530

    Add files via upload

---
+++ Pet Nest/script.js
@@ -0,0 +1,24 @@
+// JavaScript functionality can be added here
+const adoptButtons = document.querySelectorAll(".adopt-button");
+
+adoptButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+        alert("Congratulations! You've adopted a pet!");
+    });
+});
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+         }
+     });
+ });
